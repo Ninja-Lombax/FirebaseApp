e707c043fd97e0031ddb354e408ee62c934a7364
Ninja-Lombax <godzillakotm80@gmail.com>
2017-09-18 19:38:14 -0500

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/firebaseapp/MyFirebaseInstanceIDService.java
@@ -0,0 +1,22 @@
+package net.androidbootcamp.firebaseapp;
+
+
+        import android.util.Log;
+
+        import com.google.firebase.iid.FirebaseInstanceId;
+        import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh(){
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed Token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token){
+
+    }
+}
